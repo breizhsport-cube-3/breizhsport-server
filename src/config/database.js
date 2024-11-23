efe26574ee693a3d9903b2d33fd7665275d555dd
Bobin Thomas <thomas.bobin@viacesi.fr>
2024-11-23 22:15:28 +0100

    feature: Making change to connect to db in cluster

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 import { Sequelize } from 'sequelize';
 
-const DB_LOCATION = 'postgres' // Nom du service associé dans le fichier docker-compose
-const DB_NAME = 'breizhsport'
-const DB_USER = 'breizhsport'
-const DB_PASSWORD = "breizhsport"
-const DB_PORT = 5432
+const DB_LOCATION = process.env.DB_LOCATION
+const DB_NAME = process.env.DB_NAME
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_PORT = process.env.DB_PORT
 
 // Créez une instance de Sequelize et configurez la connexion à PostgreSQL
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_LOCATION}:${DB_PORT}/${DB_NAME}`)
