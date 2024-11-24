a4be6bd27b1b22ce6d2fb48078ccc127600c90f8
Bobin Thomas <thomas.bobin@viacesi.fr>
2024-11-24 14:47:50 +0100

    test: Removed props from user model

--- src/models/User.js
+++ src/models/User.js
@@ -18,50 +18,6 @@ const User = sequelize.define('User', {
   lastname: {
     type: DataTypes.STRING,
     allowNull: false,
-  },
-  avatar: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  phone_1: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  phone_2: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  mail: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  remember_token: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  creation_date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  id_group: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Group,
-      key: 'id',
-    },
-  },
-  id_address: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Address,
-      key: 'id',
-    },
   }
 });
 
